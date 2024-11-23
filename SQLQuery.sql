b20f4261e557bb1813628f8ba707eadf0a567395
Denys Kifor <kifor.denys@outlook.com>
2024-11-23 20:22:29 +0200

    check bd work

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -5,33 +5,33 @@
 USE [D:\LIBRARY\LIBRARY\DBLIBRARY.MDF];
 --GO
 
----- Таблиця [role]
---CREATE TABLE [role] (
---    id INT IDENTITY(1,1) PRIMARY KEY, -- Первинний ключ із автоінкрементом
---    name VARCHAR(30) NOT NULL         -- Додано NOT NULL для обов'язкових значень
---);
---GO
+-- Таблиця [role]
+CREATE TABLE [role] (
+    id INT IDENTITY(1,1) PRIMARY KEY, -- Первинний ключ із автоінкрементом
+    name VARCHAR(30) NOT NULL         -- Додано NOT NULL для обов'язкових значень
+);
+GO
 
----- Таблиця [user]
---CREATE TABLE [user] (
---    id INT IDENTITY(1,1) PRIMARY KEY, -- Первинний ключ із автоінкрементом
---    name VARCHAR(50) NOT NULL,        -- Ім'я користувача (обов'язкове)
---    phone VARCHAR(50),                -- Телефон (може бути NULL)
---    login VARCHAR(50) NOT NULL UNIQUE,-- Логін користувача (обов'язковий і унікальний)
---    password VARCHAR(50) NOT NULL,    -- Пароль користувача (обов'язковий)
---    roleId INT NOT NULL,              -- Роль користувача (обов'язкова)
---    FOREIGN KEY (roleId) REFERENCES [role](id) ON DELETE CASCADE ON UPDATE CASCADE
---);
---GO
+-- Таблиця [user]
+CREATE TABLE [user] (
+    id INT IDENTITY(1,1) PRIMARY KEY, -- Первинний ключ із автоінкрементом
+    name VARCHAR(50) NOT NULL,        -- Ім'я користувача (обов'язкове)
+    phone VARCHAR(50),                -- Телефон (може бути NULL)
+    login VARCHAR(50) NOT NULL UNIQUE,-- Логін користувача (обов'язковий і унікальний)
+    password VARCHAR(50) NOT NULL,    -- Пароль користувача (обов'язковий)
+    roleId INT NOT NULL,              -- Роль користувача (обов'язкова)
+    FOREIGN KEY (roleId) REFERENCES [role](id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+GO
 
---INSERT INTO [role] (name) VALUES 
---('Адміністратор'), 
---('Працівник');
+INSERT INTO [role] (name) VALUES 
+('Адміністратор'), 
+('Працівник');
 
----- Додати користувача
---INSERT INTO [user] (name, phone, login, password, roleId) 
---VALUES ('Денис', '+380688675286', 'dkifor', 'den', 1);
+-- Додати користувача
+INSERT INTO [user] (name, phone, login, password, roleId) 
+VALUES ('Денис', '+380688675286', 'dkifor', 'den', 1);
 
--- Перевірити дані
---SELECT * FROM [role];
+ --Перевірити дані
+SELECT * FROM [role];
 SELECT * FROM [user];
