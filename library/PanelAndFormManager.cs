14c960e9f264869232a48fbfb0ee2d1730fe62b4
Denys Kifor <kifor.denys@outlook.com>
2024-11-28 12:31:06 +0200

    co

--- library/PanelAndFormManager.cs
+++ library/PanelAndFormManager.cs
@@ -7,26 +7,14 @@ namespace library
     {
         public static void EmbedFormInPanel(Form childForm, Panel targetPanel)
         {
-<<<<<<< HEAD
-=======
-            // Перевірка, чи форма вже вбудована в панель
->>>>>>> 7f9592e53a4bcc03e4191fd5acca31c2eb419e5c
             foreach (Control control in targetPanel.Controls)
             {
                 if (control.GetType() == childForm.GetType()) // Перевіряємо тип форми
                 {
-<<<<<<< HEAD
                     return;
                 }
             }
 
-=======
-                    return ;
-                }
-            }
-
-            // Якщо форма ще не вбудована
->>>>>>> 7f9592e53a4bcc03e4191fd5acca31c2eb419e5c
             childForm.TopLevel = false;
             childForm.FormBorderStyle = FormBorderStyle.None;
             childForm.Dock = DockStyle.Fill;
